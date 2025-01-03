a81f3907462671f89400b92a785ae7e714e1f101
dickyalf <105033573+dickyalf@users.noreply.github.com>
2025-01-03 08:05:03 +0700

    Update DiaryAnalysisController.php

--- app/Http/Controllers/api/DiaryAnalysisController.php
+++ app/Http/Controllers/api/DiaryAnalysisController.php
@@ -8,6 +8,7 @@ use Carbon\Carbon;
 use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Log;
 
 class DiaryAnalysisController extends Controller
 {
